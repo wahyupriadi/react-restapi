8e725c99b6bfb445427e9d39daf31522531d082f
Muhammad Wahyu Priadi <muhammadwahyupriadi@xlp014027-1ad.local>
2020-08-31 18:11:51 +0700

    update editMember

--- src/pages/editMember.js
+++ src/pages/editMember.js
@@ -3,10 +3,26 @@ import { connect } from 'react-redux'
 import { wrapper } from '../redux/store'
 import axios from 'axios'
 
+export const getServerSideProps = async () => {
+    const response = await axios.get(`https://my-json-server.typicode.com/wahyupriadi/db-member/users`);
+    return {
+        props: {
+            data: response?.data,
+        },
+    };
+};
+
+// export const getServerSideProps = wrapper.getServerSideProps(
+//     ({ store, req, res, ...etc }) => {
+//         // console.log('2. Page.getServerSideProps uses the store to dispatch things');
+//         console.log(store.getState())
+//     }
+// );
+
 const editMember = ({ data, ...props }) => {
     return (
         <div>
-            <h2 className="text-xl text-center py-4">Edit Data Pengguna ID {props.id}</h2>
+            <h2 className="text-xl text-center py-4">Edit Data Pengguna ID {props}</h2>
             <form className="edit-container">
                 <div className="input-control">
                     <label className="input-label">Nama</label>
